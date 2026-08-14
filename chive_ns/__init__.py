d664c2b7a733c754bbf9b50f2f3c7c2c6766d158
Akitti <43206636+SpaceKitti@users.noreply.github.com>
2026-08-14 01:26:25 +0100

    Import additional functions from various modules

--- chive_ns/__init__.py
+++ chive_ns/__init__.py
@@ -4,3 +4,8 @@
 
 from .constants import PHI, DELTA_MIN, NU_GUM
 from .grid import make_grid, project_div_free, generate_smooth_div_free_u0
+from .vorticity import modest_liar, z7_braid_forcing, ns_vorticity_step
+from .clay import nonlinear_trilinear, gum_damping, oldroyd_b_step
+from .bubble import rayleigh_plesset, attractor_tower, coupled_rp_attractor_rhs
+from .diagnostics import lambda2_criterion
+from .driver import run_framework
